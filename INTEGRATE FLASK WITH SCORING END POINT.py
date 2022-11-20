6311f0a7c97138cdc274e5cf7988065b11409474
Pawan-2001 <113993531+Pawan-2001@users.noreply.github.com>
2022-11-20 17:19:59 +0530

    Add files via upload

---
+++ INTEGRATE FLASK WITH SCORING END POINT.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)# interface between my server and my application wsgi
+
+import pickle
+model = pickle.load(open(r'C:\Users\HP\Downloads\MultiLinear Regression\Flask app\model.pkl','rb'))
+
+@app.route('/')#binds to an url
+def helloworld():
+    return render_template("index.html")
+
+@app.route('/login')#binds to an url
+def login():
+    return render_template("login.html")
+
+@app.route('/login', methods =['POST'])#binds to an url
+def login():
+    p =request.form["ms"]
+    q= request.form["as"]
+    r= request.form["rd"]
+    s= request.form["s"]
+    if (s=="cal"):
+        s1,s2,s3=1,0,0
+    if (s=="flo"):
+        s1,s2,s3=0,1,0
+    if (s=="ny"):
+        s1,s2,s3=0,0,1
+    t=[[int(s1),int(s2),int(s3),int(p),int(q),int(r)]]
+    output= model.predict(t)
+    print(output)  
+        
+    return render_template("index.html", prediction = "the predicted profit is of your startup " + str(output[0][0]) )
+
+@app.route('/admin')#binds to an url
+def admin():
+    return "Hey Admin How are you?"
+
+if __name__ == '__main__' :
+    app.run(debug= True)
